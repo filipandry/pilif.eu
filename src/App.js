4e4a8c9e69f45fd198667a637906cb14918827b9
Filip Andrei Muresan <filip_andrey@msn.com>
2019-05-07 13:52:27 +0200

    init

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Card = props => {
+  var {name, job, email, website, linkedin} = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="card">
+        <div className="front">
+          <h1 className="title">{name}</h1>
+        </div>
+        <div className="back">
+          <h2 className="name">{name}</h2>
+          <p className="subtitle">{job}</p>
+          <div className="contacts">
+            <p><span className="contact-label">E-mail:</span> <a href={"mailto:" + email}>{email}</a></p>
+            <p><span className="contact-label">Web:</span> <a href={website}>{website}</a></p>
+            <p><span className="contact-label">Linkedin:</span> <a href={"https://www.linkedin.com/in/" + linkedin}>Profile</a></p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
+};
+
+function App() {
+  return (<Card name="Filip Andrei Muresan" linkedin="filip-andrei-muresan" email="info@pilif.eu" website="pilif.eu" job="FULL STACK DEVELOPER"/>);
 }
 
 export default App;
